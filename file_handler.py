b259cddc398cc11f870cda062f382d2554aa9647
Nkeonyelu Igboanugo <nigboanugo1@buffs.wtamu.edu>
2026-02-08 19:30:57 -0600

    writing the  logic to save the filtered dataframe to disk

--- file_handler.py
+++ file_handler.py
@@ -23,12 +23,18 @@ logging.info(f"file name : {path}")
 
 # Use Context Manager to load data
 # This ensures the file is handled safely
-with open(args.filename, 'r') as file:
-    logging.info(f"Reading file: {args.filename}")
+with open(path, 'r') as file:
+    logging.info(f"Reading file: {path}")
     df = pd.read_csv(file)
 
 
 # Filtering
 logging.info("filtering the dataframe")
 filtered_df = df[df['REPORT_TYPE'] == "FM-15"]
+
+
+# Save and Log
+output = Path("Result.csv")
+filtered_df.to_csv(output, index=False)
+logging.info(f"Done! Saved {len(filtered_df)} rows to {output}")
 
